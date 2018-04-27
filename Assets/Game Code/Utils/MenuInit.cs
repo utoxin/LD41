d96725e31416afe360d7fddabdd8b4587188e4c0
Matthew Walker <utoxin@gmail.com>
2018-04-27 07:01:07 -0600

    Tweaking menus
    Doing some work on tweaking menu layouts to improve navigation.

---
+++ Assets/Game Code/Utils/MenuInit.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Game_Code.Utils {
+	public class MenuInit : MonoBehaviour {
+		public GameObject SelectOnEnable;
+
+		private void OnEnable() {
+			EventSystem.current.SetSelectedGameObject(null);
+			EventSystem.current.SetSelectedGameObject(SelectOnEnable);
+		}
+	}
+}
